1f223876cef6f7d09ffa59a2a7e3083c9d7f2205
EHoldener <eholdener12@gmail.com>
2022-05-24 19:04:46 -0700

    created asocaition between users and posts, seeded database with two users and two posts to start

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(first_name: "Eric", last_name: "Holdener", email: "eholdener012@gmail.com", password: "123456", username: "Eric")
+User.create(first_name: "John", last_name: "Doe", email: "johndoe@gmail.com", password: "123456", username: "BigJD")
+
+Post.create(title: "My first post here!", content: "I'm excited to be working on this facebook clone", user_id: 1)
+Post.create(title: "Please Help Me", content: "I don't know who I am", user_id: 2)
