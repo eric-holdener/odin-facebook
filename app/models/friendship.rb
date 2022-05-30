77191bbd51a0bfb7f9f7611ce667f6ac02f39e1f
EHoldener <eholdener12@gmail.com>
2022-05-30 14:32:09 -0700

    formatting issue for postgre fix, i think

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,5 +2,5 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 
-  scope :accepted_friend, -> { where('status == ?', 1) }
+  scope :accepted_friend, -> { where('status = ?', 1) }
 end
